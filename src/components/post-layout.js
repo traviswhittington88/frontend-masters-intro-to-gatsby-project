6edc89be43a23a0a3c05d5280474e7b90e0f2a41
Travis H Whittington <TWhittington@woodmenlife.org>
2022-05-02 14:46:12 -0500

    made a back button and link to other blog posts

--- src/components/post-layout.js
+++ src/components/post-layout.js
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { Link } from 'gatsby'; // for link to full listing
 import Layout from './layout.js'; // get the content of mdx and insert the details into this layout component
 
-export default function PostLayout({ children , pageContext }) {
+export default function PostLayout({ children, pageContext }) {
     const { title, description } = pageContext.frontmatter;
     return (
         <Layout
-					title={title}
-					description={description}
-				>
-          {children}
-					<Link to="/">&larr; back</Link>
-        </Layout>   
+            title={title}
+            description={description}
+        >
+            {children}
+            <Link to="/">&larr; back</Link>
+        </Layout>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,38 @@ import { Link, useStaticQuery, graphql } from 'gatsby'; // useStaticQuery is a h
 import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+	const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            date(fromNow: true)
+            description
+            title
+          }
+        }
+      }
+    }
+  `);
 
-    return (
-    <Layout>
-    <h1>Hello Frontend Masters</h1>
-    <Link to="/about">About Me</Link>
-    </Layout>
-    );
+	const posts = data.allMdx.nodes;
+
+	return (
+		<Layout>
+			<h1>Hello Frontend Masters</h1>
+			<Link to="/about">About Me</Link>
+
+			<h2>Check out my recent blog posts</h2>
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>
+						<Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+						<small>posted {post.frontmatter.date}</small>
+					</li>
+				))}
+			</ul>
+		</Layout>
+	);
 }
